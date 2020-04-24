b74b8d9cd47a9c236a0d733616962ba81d2baa9a
Bashir <bashiralhanshali@yahoo.com>
2020-04-24 13:58:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -87,6 +87,12 @@ def adding_matz
         :languages => ["C"]
       }
     }
+    :yukihiro_matsumoto => {
+#   :known_for => "Ruby",
+#   :languages => ["LISP", "C"]
+# }
+    
+    programmer_hash[:yukihiro_matsumoto] = 
 
 end
 
